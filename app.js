de2e2ffa044c4c4013342712694f87a62438dbe2
LuisRZJ <luis213465rivas@gmail.com>
2026-06-13 17:53:23 -0600

    Corrección menor a la fecha de actualización.

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const isSupported = () => {
 
 // variables de versión de página
 window.PAGE_VERSION = "6.10.0";
-window.ACT_DATE = "08/06/2026";
+window.ACT_DATE = "13/06/2026";
 
 console.log("Página versión: " + window.PAGE_VERSION + ", actualizada por ultima vez el: " + window.ACT_DATE);
 
