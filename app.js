6d6bc9cbb51d9617be3ab5ac9959cec2f5edde44
LuisRZJ <luis213465rivas@gmail.com>
2026-07-09 16:36:00 -0600

    ## modulo-inversiones/cryptotab
    - Replica de la mejora de Bloquear ganancia (casilla de verificacion) en los modales de edicion del registro v2 mas reciente para ct-browser.html, ct-farm.html y ct-nft.html.
    - En ct-browser.html (multi-cuenta) el bloqueo congela las ganancias parciales (sats_lite, sats_pro, sats_max) y la ganancia total (sats) e impide su recalculo al modificar los saldos totales acumulados de cada cuenta.
    - En ct-farm.html y ct-nft.html (cuenta unica) la logica replica exactamente el bloqueo e inmunidad al cambio de total.
    
    ## general
    
    - Incremento de version a v6.14.11 y fecha de actualizacion al 09/07/2026 en app.js.

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const isSupported = () => {
 
 
 // variables de versión de página
-window.PAGE_VERSION = "6.14.10";
+window.PAGE_VERSION = "6.14.11";
 window.ACT_DATE = "09/07/2026";
 
 console.log("Página versión: " + window.PAGE_VERSION + ", actualizada por ultima vez el: " + window.ACT_DATE);
